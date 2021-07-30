f57014f27e5e859a77070f7d552dd71548815890
Maheboob Ali Kankurti <mahebok1@amdocs.com>
2021-07-30 08:15:57 +0530

    form based authentication (thymleaf syntax is not working , need to check more )

--- src/main/java/com/learn/config/MySecurityConfig.java
+++ src/main/java/com/learn/config/MySecurityConfig.java
@@ -19,12 +19,16 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter
      http
              .csrf().disable()
              .authorizeRequests()
+             .antMatchers(("/signin")).permitAll()
              .antMatchers("/public/**").hasRole("NORMAL")
              .antMatchers("/users/**").hasRole("ADMIN")
              .anyRequest()
              .authenticated()
              .and()
-             .httpBasic();
+             .formLogin()
+             .loginPage("/signin")
+             .loginProcessingUrl("/dologin")
+             .defaultSuccessUrl("/users/");
     }
 
     //assigning custom username ,password and roles
---
+++ src/main/java/com/learn/contorllers/TestController.java
@@ -0,0 +1,15 @@
+package com.learn.contorllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class TestController
+{
+    @GetMapping("/signin")
+    public  String signin()
+    {
+        return "login.html";
+    }
+}
